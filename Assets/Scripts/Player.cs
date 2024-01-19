ee6f81a9c3ff8dd214a181fe3c0fa0cf672142f8
WilliamMarroush <marroushw@yahoo.com>
2024-01-18 20:30:39 -0500

    Initial Unity Commit1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D playerrb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerrb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move(){
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(horizontal, vertical);
+        playerrb.velocity = movement * 5f;   
+    }
+}
